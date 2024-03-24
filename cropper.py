63be6fc85dad0f02e9d40fc377eca1882dd3c005
Brendan Richards <brenrichrads@byui.edu>
2024-03-23 21:16:19 -0600

    prints num of pixels matching pattern

--- cropper.py
+++ cropper.py
@@ -2,11 +2,16 @@ from PIL import Image
 import random
 
 def cropper(img, width, height, keepColor):
+    matchingPixels = 0
 
     for x in range(0, width):
         for y in range(0, height):
             selected = img.getpixel((x, y))
             if selected != keepColor:
                 img.putpixel((x, y), (0, 0, 0, 0))
+            else:
+                matchingPixels = matchingPixels + 1
+    
+    print(f"Matching Pixles: {matchingPixels}")
 
     return img
