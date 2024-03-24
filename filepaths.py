c045c3e802bc6dcae05c6b3e3d6647469d9d05c4
Brendan Richards <brenrichrads@byui.edu>
2024-03-23 21:16:51 -0600

    removed test prints and added "extension support"

--- filepaths.py
+++ filepaths.py
@@ -1,29 +1,22 @@
 def outputpath(input_image_path):
-    print(input_image_path)
     # Split the input image by /
     splitList = input_image_path.split('/')
-    print(splitList)
 
     # Image name is the last element of the splitList list
     imagename = splitList[-1]
-    print(imagename)
 
     # Split imageName by .
     tmpList = imagename.split('.')
     imageNoExt = tmpList[0]
-    print(imageNoExt)
+    ext = '.' + tmpList[1]
 
     # Chop off the last element from splitList
     slicedList= splitList[:-1]
-    print(slicedList)
 
     # Join all elements of splitList with /
     slicedList = '/'.join(slicedList)
-    print(slicedList)
 
     # Add a folder to slicedList with /
     slicedList = slicedList + '/'
-    print(slicedList)
-
-
-    return slicedList + imageNoExt + "_modified.png"
+    
+    return slicedList + imageNoExt + "_modified" + ext
